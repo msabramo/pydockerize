8a9061b96c7934ed5975ebc4a5fa92383378ab92
Marc Abramowitz <marc@marc-abramowitz.com>
2015-02-12 15:54:35 -0800

    Ability to build and tag for multiple py versions

--- pydockerize.py
+++ pydockerize.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
+import contextlib
 import os
+import shutil
 import subprocess
 import sys
 import textwrap
@@ -14,7 +18,8 @@ DEFAULT_BASE_IMAGES = ['python:2-onbuild']
 @click.command()
 @click.version_option()
 @click.option('-b', '--base-images',
-              help='Base docker image')
+              help='Base docker images (comma-separated list) - e.g.: '
+                   '"python:2.7-onbuild,python:3.4-onbuild"')
 @click.option('-t', '--tag',
               help='Repository name (and optionally a tag) to be applied to '
                    'the resulting image in case of success')
@@ -26,16 +31,18 @@ def pydockerize(ctx, requirements_file, tag, base_images=DEFAULT_BASE_IMAGES):
     print('requirements_file = %r' % requirements_file)
     print('tag = %r' % tag)
     for base_image in base_images.split(','):
-        write_dockerfile(base_image)
-    invoke_docker_build(tag)
+        filename = write_dockerfile(base_image, requirements_file)
+        invoke_docker_build(tag, base_image, filename)
+
+    print('\nShowing Docker images for %s:\n' % tag)
+    show_docker_images(tag)
+    print()
 
 
-def write_dockerfile(base_image):
+def write_dockerfile(base_image, requirements_file):
     print('write_dockerfile: base_image = %r' % base_image)
-    filename = 'pydockerize_output/{base_image}/Dockerfile'.format(
-        base_image=base_image)
-    if not os.path.isdir(os.path.dirname(filename)):
-        os.makedirs(os.path.dirname(filename))
+    dirname = '.'
+    filename = os.path.join(dirname, 'Dockerfile-' + base_image)
     print('write_dockerfile: Writing %s' % filename)
 
     with open(filename, 'w+') as f:
@@ -55,13 +62,20 @@ def write_dockerfile(base_image):
             WORKDIR /host
         """.format(base_image=base_image)))
 
+    return filename
 
-def invoke_docker_build(tag):
-    print('invoke_docker_build: tag = %r' % tag)
+
+def invoke_docker_build(repo_and_tag, base_image, filename):
+    print('invoke_docker_build: repo_and_tag = %r' % repo_and_tag)
     cmd = ['docker', 'build']
-    if tag:
+    if repo_and_tag:
+        if ':' in repo_and_tag:
+            raise Exception("':' in tag not supported yet")
+        tag = get_tag_from_base_image(base_image)
         cmd.append('-t')
-        cmd.append(tag)
+        cmd.append(repo_and_tag + ':' + tag)
+    cmd.append('--file')
+    cmd.append(filename)
     cmd.append('.')
     print('invoke_docker_build: Calling subprocess with cmd = %r' % cmd)
     status = subprocess.call(cmd)
@@ -69,6 +83,21 @@ def invoke_docker_build(tag):
         print('Docker build succeeded.')
     else:
         print('Docker build failed with %d' % status)
+
+
+def show_docker_images(repo):
+    cmd = ['docker', 'images', repo]
+    return subprocess.call(cmd)
+
+
+def get_tag_from_base_image(base_image):
+    tag = base_image
+    replacements = {'python:': 'py', '-onbuild': ''}
+
+    for old, new in replacements.items():
+        tag = tag.replace(old, new)
+
+    return tag
 
 
 if __name__ == '__main__':
