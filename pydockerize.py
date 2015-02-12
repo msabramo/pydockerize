a76169f6dd47106f81985f659495f9af33fac560
Marc Abramowitz <marc@marc-abramowitz.com>
2015-02-12 14:27:11 -0800

    More support for specifying base image

--- pydockerize.py
+++ pydockerize.py
@@ -8,28 +8,30 @@ import textwrap
 import click
 
 
-DEFAULT_BASE_IMAGE = 'python:2-onbuild'
+DEFAULT_BASE_IMAGES = ['python:2-onbuild']
 
 
 @click.command()
 @click.version_option()
+@click.option('-b', '--base-images',
+              help='Base docker image')
 @click.option('-t', '--tag',
               help='Repository name (and optionally a tag) to be applied to '
                    'the resulting image in case of success')
 @click.argument('requirements_file', type=click.Path(exists=True),
                 default='requirements.txt')
 @click.pass_context
-def pydockerize(ctx, requirements_file, tag):
+def pydockerize(ctx, requirements_file, tag, base_images=DEFAULT_BASE_IMAGES):
     """Create Docker images for Python apps"""
     print('requirements_file = %r' % requirements_file)
     print('tag = %r' % tag)
-    if not os.path.exists('Dockerfile'):
-        write_dockerfile()
+    for base_image in base_images.split(','):
+        write_dockerfile(base_image)
     invoke_docker_build(tag)
 
 
-def write_dockerfile(base_image=DEFAULT_BASE_IMAGE):
-    print('write_dockerfile')
+def write_dockerfile(base_image):
+    print('write_dockerfile: base_image = %r' % base_image)
     with open('Dockerfile', 'w') as f:
         f.write(textwrap.dedent("""\
             # This Docker image takes care of doing `pip install -r requirements.txt`
